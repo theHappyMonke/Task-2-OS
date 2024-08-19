3f8256006e847158007e3a50de4cf627e524aa89
theHappyMonke <23DelmarAS271@collyers.ac.uk>
2024-08-19 16:54:58 +0100

    re-jig, getting everything working

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from flask import Flask, render_template
+import sqlite3
+
+connection = sqlite3.connect('BeanBrew.db', check_same_thread = False)
+
+cursor = connection.cursor()
+
+query = """
+CREATE TABLE IF NOT EXISTS 
+users(id INTEGER PRIMARY KEY, 
+name TEXT NOT NULL, 
+email TEXT NOT NULL UNIQUE, 
+password TEXT NOT NULL)
+"""
+cursor.execute(query)
+cursor.close()
 
 app = Flask(__name__)
 
@@ -22,5 +37,9 @@ def table():
 def classes():
     return render_template('class-booking.html')
 
+@app.route('/sign-in')
+def sign_in():
+    return render_template('sign-in.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug = True)
